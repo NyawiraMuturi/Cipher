150d62b279e9154912ca3f554f6e34cee4a306e3
Nyawira Muturi <albinamuturi@gmail.com>
2020-04-23 10:37:53 +0300

    add changes made to App.java

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -7,13 +7,13 @@ public class CipherTest {
     @Test
     public void Cipher_testingEncryptingmethod() {
         Cipher cipher =new Cipher();
-        assertEquals("nbe",cipher.encrypt("mad",1));
+        assertEquals("eqy",cipher.encrypt("cow",2));
     }
 
     @Test
     public void Cipher_testingDecryptingMethod() {
         Cipher cipher =new Cipher();
-        assertEquals("mad",cipher.decrypt("nbe",1));
+        assertEquals("cow",cipher.decrypt("eqy",2));
     }
 
 
